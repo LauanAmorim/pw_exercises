64667761ac46027741a6c7f6bb1e1731a3ee0d1a
Lauan Amorim <lauan12x@gmail.com>
2023-03-12 19:33:42 -0300

    1-4 exercises

---
+++ ExeLauElisI/Controllers/AulaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExeLauElisI.Controllers
+{
+    public class AulaController : Controller
+    {
+        // GET: Aula
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MenuRedirect()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
